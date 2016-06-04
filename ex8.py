e1e89670f413cc30eb343d6e26a37552c78a71b2
jxnguyen <johnxuannguyen@gmail.com>
2016-06-04 11:32:50 +0200

    weave_list -> merge_lists

--- ex8.py
+++ ex8.py
@@ -1,36 +1,31 @@
+from collections import namedtuple
+
 
 def conversion_table(rate = 1):
 	'''
 	Generate currency conversion table between curr & euros with the given conversion rate (amount of euros for a single unit of curr).
 	'''
+	# structure to pair currency values
+	Conversion = namedtuple('Conversion', ['foreign','euro'])
 	# denominations
-	denoms = [d * 10**e for d in [1,2,5] for e in range(6)]
+	denoms = [d * 10**e for e in range(6) for d in [1,2,5]]
 	# euro denominations
 	e_denoms = list(filter(lambda e: e <= 100, denoms))
 	# (curr, euro) values for euro denoms
-	from_euros = [(e * rate, e) for e in e_denoms]
+	from_euros = [Conversion(e * rate, e) for e in e_denoms]
 	# (curr, euro) values for currency denoms
-	curr_values = [c, (c * (1/rate)) for c in denoms]
+	curr_values = [Conversion(c, c * (1/rate)) for c in denoms]
 	# weave lists
-	return weave_lists(from_euros, from_curr)
+	return merge_lists(from_euros, curr_values)
 
 def print_table(currency, table):
 	'''
 	Print conversion table to screen.
 	'''
 	print()
-	for curr, euro in table:
-		print('{:10.2f} {} = {:6.2f} EUR'.format(curr, currency, euro))
+	for e in table:
+		print('{:10.2f} {} = {:6.2f} EUR'.format(e.foreign, currency, e.euro))
 	print()
-
-def gen_c_denoms(info):
-	'''
-	Returns a list of appropriate denominations of currency with respect to the Euro denominations.
-	'''
-	# retrieve conversion amounts for each euro denom
-	_, amounts = zip(*info)
-	# calculate midpoints between amounts & round
-	return [d_round(a * 1.5) for a in amounts[:-1]]
 
 def d_round(n):
 	'''
@@ -44,29 +39,22 @@ def d_round(n):
 	# divide, round, multiply
 	return round(int(x)/size)*size
 
-def weave_lists(A, B):
+def merge_lists(A, B):
 	'''
-	Return the list produced by alternating elements from A and B. Alternation
-	starts with A and ends when either list is exhausted. Any remaining elements
-	are concatenated afterwards.
-	Example: [a,b,c,d,e] & [1,2,3] -> [a,1,b,2,c,3,d,e]
+	Merge the list B within the list A in ascending order. The resulting list starts & ends with the first & last elements of A. Ex. [a1, b1,...., bn, am]
 	'''
 	l = list()
-	i = 0
-	# while i not overbounds
-	while i < len(A) and i < len(B):
-		# alternate appends
-		l.append(A[i])
-		l.append(B[i])
-		i += 1
-
-	# concatenate remaing elements if any
-	if i < len(A): l += A[i:]
-	if i < len(B): l += B[i:]
+	# element pairs from A
+	pairs = zip(A[:-1], A[1:])
+	# for each pair in A
+	for x, y in pairs:
+		# add first element
+		l.append(x)
+		# add successive B elements, up to next euro amount
+		l += filter(lambda e: x.foreign < e.foreign <= y.foreign, B)
+	# cap the end with last elem of A
+	l.append(A[-1])
 	return l
-
-
-
 
 
 currency = input('Currency: ')
